06bb77a7b90b66e11c4ae26ad10ef06373ea9f7f
ltpie123 <ltpie12345+github@gmail.com>
2024-11-25 10:24:31 -0800

    Updated synth_tb

--- final_project_1_5/final_project_1_5.srcs/sim_1/imports/new/synth_tb.v
+++ final_project_1_5/final_project_1_5.srcs/sim_1/imports/new/synth_tb.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
+// Company:
+// Engineer:
+//
 // Create Date: 10/23/2024 09:30:52 PM
-// Design Name: 
+// Design Name:
 // Module Name: synth_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
+// Project Name:
+// Target Devices:
+// Tool Versions:
+// Description:
+//
+// Dependencies:
+//
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -27,7 +27,7 @@ module synth_tb;
     reg [6:0] midi_in;
     reg [1:0] wav_sel;
     wire [7:0] wav_out;
-    
+
     synth uut (
         .clk(clk),
         .reset(reset),
@@ -35,61 +35,61 @@ module synth_tb;
         .wav_sel(wav_sel),
         .wav_out(wav_out)
     );
-    
+
     initial begin
         clk = 0;
         forever #10 clk = ~clk;
     end
-    
+
     initial begin
         reset = 1;
-        
+
         #20
-        
+
         reset = 0;
-        
+
         wav_sel = 2'b00;
         midi_in = 7'd60;
         #1000;
-        
+
         midi_in = 7'd72;
         #1000;
-        
+
         midi_in = 7'd48;
         #1000;
-        
+
         wav_sel = 2'b01;
         midi_in = 7'd60;
         #1000;
-        
+
         midi_in = 7'd72;
         #1000;
-        
+
         midi_in = 7'd48;
         #1000;
-        
+
         wav_sel = 2'b10;
         midi_in = 7'd60;
         #1000;
-        
+
         midi_in = 7'd72;
         #1000;
-        
+
         midi_in = 7'd48;
         #1000;
-        
+
         wav_sel = 2'b11;
         midi_in = 7'd60;
         #1000;
-        
+
         midi_in = 7'd72;
         #1000;
-        
+
         midi_in = 7'd48;
         #1000;
-        
+
         reset = 1;
-        
+
         $finish;
-    end 
+    end
 endmodule
